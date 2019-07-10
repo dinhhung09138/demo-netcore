2201c3c68c1044d08c5e5d556fac4acbc9966f4c
Hung Tran <hung.tran@tpptechnology.com>
2019-07-10 17:18:25 +0700

    Using jquery datatable in .Net Core MVC

---
+++ NetCoreMVCAndJqueryDataTable/NetCoreMVCAndJqueryDataTable/Utils/JqueryDatatable/DataTableRequestHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVCAndJqueryDataTable.Utils.JqueryDatatable
+{
+    /// <summary>
+    /// Jquery datatable Request from client
+    /// </summary>
+    public class DataTableRequestHelper
+    {
+        /// <summary>
+        /// draw
+        /// </summary>
+        public int draw { get; set; }
+
+        /// <summary>
+        /// start
+        /// </summary>
+        public int start { get; set; }
+
+        /// <summary>
+        /// length
+        /// </summary>
+        public int length { get; set; }
+
+        /// <summary>
+        /// order
+        /// </summary>
+        public List<OrderingHelper> order { get; set; }
+
+        /// <summary>
+        /// search
+        /// </summary>
+        public SearchHelper search { get; set; }
+
+        /// <summary>
+        /// columns
+        /// </summary>
+        public List<ColumnHelper> columns { get; set; }
+    }
+}
---
+++ NetCoreMVCAndJqueryDataTable/NetCoreMVCAndJqueryDataTable/Utils/JqueryDatatable/SortingDirectionHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVCAndJqueryDataTable.Utils.JqueryDatatable
+{
+    /// <summary>
+    /// Sort direction
+    /// </summary>
+    public enum SortingDirectionHelper
+    {
+        /// <summary>
+        /// Ascessding
+        /// </summary>
+        Asc,
+
+        /// <summary>
+        /// Deacessding
+        /// </summary>
+        Desc
+    }
+}
